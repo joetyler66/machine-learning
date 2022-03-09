12745e31174e9f12c259b569676dd9a11c7392cf
joetyler66 <98910085+joetyler66@users.noreply.github.com>
2022-03-09 16:39:59 +0000

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -1,11 +1,18 @@
-'''
+"""
+perceptron.py
+"""
 
-
+#Libraries used
 import numpy as np 
+import random
 
 class Network: #(object):
 
   def __init__(self, sizes):
+    '''
+    initialise network with sizes[0] inputs, sizes[i] neurons in the ith hidden layer and sizes[end]
+    weights and biases are initially randomized with a normal distribution
+    '''
     self.num_layers = len(sizes)
     self.sizes = sizes
     self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
