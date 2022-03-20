561754b929e3ba3d94879dc2a547827fbe63ed68
joetyler66 <joetyler66@hotmail.com>
2022-03-20 21:26:45 +0000

    Update perceptron.py

--- perceptron.py
+++ perceptron.py
@@ -25,21 +25,34 @@ class Network: #(object):
       a = sigmoid(np.dot(w,a)+b)
     return a
 
-  def SGD(self, training_data, epochs, mini_batch_size, rate, test_data=None):
+  def SGD(self, training_data, repeats, mini_batch_size, rate, test_data=None):
     """trains the neural network with stochastic gradient descent
     testing not implemented yet"""
     n=len(training_data)
-    for j in range(epochs):
+    for j in range(repeats):
       random.shuffle(training_data)
       mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
       for mini_batch in mini_batches:
         self.update_mini_batch(mini_batch, rate)
-      print('Epoch {0} complete'.format(j))
+      print('Repeat {0} complete'.format(j))
     
   def update_mini_batch(self, mini_batch, rate):
+    """update weights and biases by applying gradient descent using backpropagation to a single mini batch of input output tuples"""
+    # initialize changes to biases and weights
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    # calculate contribution to backpropagation from each input and sum
+    for x, y in mini_batch:
+      delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+      nabla_b=[nb+d for nb, d in zip(nabla_b, delta_nabla_b)]
+      nabla_w=[nw+d for nw, d in zip(nabla_w,delta_nabla_w)]
     return 
 
-    
+  def backprop(self, x, y):
+    """"Return a tuple ''(nabla_b,nabla_w)'' representing the gradient of the cost function"""
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    return
 
 #General functions used
 def sigmoid(z):
@@ -49,7 +62,3 @@ def sigmoid(z):
 def sigmoid_prime(z):
   """Derivative of the sigmoid function."""
   return sigmoid(z)*(1-sigmoid(z))
-
-x=Network([2,3,2])
-print(x)
-print(x.feedforward(np.array([1,1])))
